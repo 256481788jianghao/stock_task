99b3743fd0a85f9caa64eccefd0d6a79ea612ee3
jianghao <256481788@163.com>
2018-11-10 11:22:16 +0800

    up 2

--- porting.py
+++ porting.py
@@ -35,6 +35,56 @@ pretrade_date	str	上一个交易日
 def trade_cal():
     return pro_api.trade_cal()
 
-data = trade_cal()
+#get daily date
+'''
+ts_code	str	股票代码
+trade_date	str	交易日期
+open	float	开盘价
+high	float	最高价
+low	float	最低价
+close	float	收盘价
+pre_close	float	昨收价
+change	float	涨跌额
+pct_change	float	涨跌幅
+vol	float	成交量 （手）
+amount	float	成交额 （千元）
+'''
+def daily(trade_date):
+    return pro_api.daily(trade_date = trade_date)
+
+#get adj_factor
+'''
+ts_code	str	股票代码
+trade_date	str	交易日期
+adj_factor	float	复权因子
+start_date	str	开始日期
+end_date	str	结束日期
+'''
+def adj_factor(ts_code):
+    return pro_api.adj_factor(ts_code = ts_code)
+
+
+#get daily_basic
+'''
+ts_code	str	TS股票代码
+trade_date	str	交易日期
+close	float	当日收盘价
+turnover_rate	float	换手率
+turnover_rate_f	float	换手率（自由流通股）
+volume_ratio	float	量比
+pe	float	市盈率（总市值/净利润）
+pe_ttm	float	市盈率（TTM）
+pb	float	市净率（总市值/净资产）
+ps	float	市销率
+ps_ttm	float	市销率（TTM）
+total_share	float	总股本 （万）
+float_share	float	流通股本 （万）
+free_share	float	自由流通股本 （万）
+total_mv	float	总市值 （万元）
+circ_mv	float	流通市值（万元）
+'''
+def daily_basic(trade_date):
+    return pro_api.daily_basic(trade_date = trade_date)
+data = daily_basic('20170711')
 
 print(data)
