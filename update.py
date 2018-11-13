30a9cf014fef09b68a634c922b7513f767f9828f
jianghao <2564811788@163.com>
2018-11-13 13:28:40 +0800

    fix

--- update.py
+++ update.py
@@ -4,9 +4,13 @@ import sqlite3 as sql
 
 sql_con = sql.connect('stock.db')
 try:
+    print('start update stock basic')
     data = pt.stock_basic()
-    print(data)
-except e:
+    if type(data) == pd.DataFrame:
+        data.to_sql('stock_basic',sql_con,if_exists='replace')
+    else:
+        print('get stock_basic faild')
+except Exception as e:
     print(e)
 finally:
     print("end update")
