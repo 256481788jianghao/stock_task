78a379b53fc4137bf905ddabd200cb02d7cce742
jianghao <2564811788@163.com>
2019-04-09 16:20:04 +0800

    ddsdf

--- FunctionMgr.py
+++ FunctionMgr.py
@@ -25,6 +25,7 @@ class FunctionMgr:
             tmp = dict()
             tmp['mean_rate_f'] = item.turnover_rate_f.mean()
             tmp['mean_rate'] = item.turnover_rate.mean()
+            tmp['mean_count'] = len(item)
             return pd.Series(tmp)
         ans = group.apply(func)
         return (ans)
@@ -82,6 +83,7 @@ class FunctionMgr:
             for p in item['pct_change']:
                 p_all = p_all*(1+p/100)
             tmp['sum_pct_change'] = (p_all-1)*100
+            tmp['sum_count'] = len(item)
             return pd.Series(tmp)
         ans = group.apply(func)
         return ans
@@ -103,13 +105,51 @@ class FunctionMgr:
         merge_data = pd.merge(left=mean_data,right=concept_data,left_index=True,right_index=True)
         #print(sum_pctchange_data)
         return merge_data
-    
+    '''
+           获得换手率与价格浮动的关系表，截止到某日
+    '''
+    def GetPctChangeAndTurnoverRateRelationList(self,end_date,al=5,bs=5,bl=10):
+        date_all_len = al+bl
+        date_list = pd.read_sql_query('select * from trade_cal where is_open = 1 and cal_date <="'+str(end_date)+'"',self.sql_con)
+        date_list_all = date_list[-date_all_len:]
+        pdate = date_list_all.cal_date.iloc[-al]
+        sdate_bl = date_list_all.cal_date.iloc[0]
+        sdate_bs = date_list_all.cal_date.iloc[bs]
+        data_turnover = pd.read_sql_query('select ts_code,trade_date,turnover_rate,turnover_rate_f from daily_basic where trade_date <='+str(end_date)+' and trade_date >='+str(sdate_bl), self.sql_con)
+        data_daily = pd.read_sql_query('select trade_date,ts_code,pct_change from daily where trade_date >= '+str(sdate_bl)+' and trade_date <= '+str(end_date),self.sql_con)
+        merge_data = pd.merge(left=data_daily, left_on=['ts_code','trade_date'],right=data_turnover, right_on=['ts_code','trade_date'])
+        def sum_pct_change(data):
+            ans_sum = 1
+            for item in data:
+                ans_sum = ans_sum * (1+item/100)
+            return ans_sum
+        merge_date_group = merge_data.groupby('ts_code')
+        def g_func(items):
+            tmp= dict()
+            data_bl = items[items.trade_date < pdate]
+            data_bs = items[(items.trade_date <pdate) & (items.trade_date >= sdate_bs)]
+            data_al = items[items.trade_date >= pdate]
+            tmp['mean_turnover_f_bl'+str(bl)] = data_bl.turnover_rate_f.mean()
+            tmp['mean_turnover_f_bs'+str(bs)] = data_bs.turnover_rate_f.mean()
+            tmp['sum_pchaneg_bl'+str(bl)] = sum_pct_change(data_bl['pct_change'])
+            tmp['sum_pchaneg_al'+str(al)] = sum_pct_change(data_al['pct_change'])
+            tmp['data_len'] = len(items)
+            return pd.Series(tmp)
+        ans_data = merge_date_group.apply(g_func)
+        sub_ans_data = ans_data[ans_data.data_len >= date_all_len]
+        sub_ans_data['t_rate'] = sub_ans_data['mean_turnover_f_bs'+str(bs)]/sub_ans_data['mean_turnover_f_bl'+str(bl)]
+        sort_data = sub_ans_data.sort_values(by='sum_pchaneg_al'+str(al),ascending=False)
+        print(sort_data[(sort_data.t_rate > 1.3) & (sort_data.sum_pchaneg_al5 < 1)])
+        
+        
 if __name__ == '__main__':
     pd.set_option('max_columns', 100)
     with sql.connect('stock.db') as con:
         mgr = FunctionMgr(con)
-        data_concept_mean = mgr.GetSumPChangeAndMeanTurnoverRateList( 20190101, 20190403)
-        print(data_concept_mean.sort_values(by='sum_pct_change',ascending=False))
+        mgr.GetPctChangeAndTurnoverRateRelationList(20190401)
+        #data_concept_mean = mgr.GetSumPChangeAndMeanTurnoverRateList( 20190401, 20190403)
+        #data_mean = data_concept_mean.sort_values(by='sum_pct_change',ascending=False)
+        #print(mgr.GetConceptSortList(data_mean[0:21].index))
         #data = mgr.GetTurnoverRateMeanSortList(20190120,20190329)
         #data_sort = data.sort_values(by='mean_rate_f',ascending=False)
         #concept_data = mgr.GetConceptSortList(data_sort[0:21].index)
--- update.py
+++ update.py
@@ -11,7 +11,7 @@ sql_con = sql.connect('stock.db')
 cursor = sql_con.cursor()
 
 start_date = '20170101'
-end_date = '20190403'
+end_date = '20190408'
 now_date = datetime.datetime.now().strftime('%Y%m%d')
 
 tables_info = None
