9fa2fd121679ea431ef3f461b49abc165aae598c
jianghao <2564811788@163.com>
2020-01-02 08:10:00 +0800

    add qt

---
+++ pyqtTest.py
@@ -0,0 +1,13 @@
+'''
+Created on 2019年12月30日
+
+@author: Administrator
+'''
+
+from PyQt5 import QtWidgets, QtGui
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget();
+window.show()
+sys.exit(app.exec_())
--- update.py
+++ update.py
@@ -339,3 +339,6 @@ def UpdateFunction(start_date,end_date):
         print("end update")
         cursor.close()
         sql_con.close()
+
+if __name__ == '__main__':
+    UpdateFunction('20190101', '20191230')
